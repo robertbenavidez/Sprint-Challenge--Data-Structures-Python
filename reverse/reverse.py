5b94f673a26fd596e53b14109931f0f37c0f4a90
Robert Benavidez <murabitbenavidez@gmail.com>
2020-08-29 13:27:58 -0400

    reverse passing all tests

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,10 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        current = self.head
+        while current is not None:
+            next_node = current.next_node
+            current.next_node = prev
+            prev = current
+            current = next_node
+            self.head = prev
