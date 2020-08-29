20ec85c31bdb68d63689d82e58e6a19aeb9e427c
Robert Benavidez <murabitbenavidez@gmail.com>
2020-08-29 14:37:19 -0400

    MVP Met: ring buffer passing all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,44 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.current == None:
+            self.current = self.storage.head
+
+        if self.capacity == self.storage.length and self.current == self.storage.head:
+            self.storage.remove_from_head()
+            self.storage.add_to_head(item)
+            self.current = self.current.next
+
+        elif self.capacity == self.storage.length and self.current == self.storage.tail:
+            self.storage.remove_from_tail()
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+
+        elif self.capacity == self.storage.length:
+            self.current.insert_after(item)
+            self.storage.length += 1
+            self.storage.delete(self.current)
+            self.current = self.current.next.next
+
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
-        pass
+        buffer_items = []
+        temp = self.storage.head
+
+        if temp is None:
+            return buffer_items
+        else:
+            while temp is not None:
+                buffer_items.append(temp.value)
+                temp = temp.next
+        return buffer_items
+
